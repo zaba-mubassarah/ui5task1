f70b978b0f84f1334a8632659e3b49e88397d5a3
zaba <zaba.mubassarah@gmail.com>
2022-10-12 18:17:11 -1200

    up

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,33 +1,31 @@
+/**
+ * eslint-disable @sap/ui5-jsdocs/no-jsdoc
+ */
+
 sap.ui.define(
-  [
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  function (UIComponent, JSONModel, ResourceModel) {
+  ["sap/ui/core/UIComponent", "sap/ui/Device", "task/shanita/model/models"],
+  function (UIComponent, Device, models) {
     "use strict";
+
     return UIComponent.extend("task.shanita.Component", {
       metadata: {
-        interfaces: ["sap.ui.core.IAsyncContentCreation"],
         manifest: "json",
       },
-      init: function () {
-        // call the init function of the parent
-        UIComponent.prototype.init.apply(this, arguments);
-        // set data model
-        var oData = {
-          recipient: {
-            name: "World",
-          },
-        };
-        var oModel = new JSONModel(oData);
-        this.setModel(oModel);
 
-        // set i18n model
-        var i18nModel = new ResourceModel({
-          bundleName: "task.shanita.i18n.i18n",
-        });
-        this.setModel(i18nModel, "i18n");
+      /**
+       * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+       * @public
+       * @override
+       */
+      init: function () {
+        // call the base component's init function
+        UIComponent.prototype.init.apply(this, arguments);
+
+        // enable routing
+        this.getRouter().initialize();
+
+        // set the device model
+        this.setModel(models.createDeviceModel(), "device");
       },
     });
   }
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,17 +1,182 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/base/Log",
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/core/format/DateFormat",
+    "sap/m/ToolbarSpacer",
+    "sap/ui/thirdparty/jquery",
+  ],
+  function (
+    Log,
+    Controller,
+    JSONModel,
+    Filter,
+    FilterOperator,
+    DateFormat,
+    ToolbarSpacer,
+    jQuery
+  ) {
     "use strict";
-    return Controller.extend("task.shanita.controller.App", {
-      onShowHello: function () {
-        // read msg from i18n model
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        // show message
-        MessageToast.show(sMsg);
+
+    return Controller.extend("sap.ui.table.sample.Filtering.Controller", {
+      onInit: function () {
+        var oView = this.getView();
+
+        // set explored app's demo model on this sample
+        var oJSONModel = this.initSampleDataModel();
+        oView.setModel(oJSONModel);
+
+        oView.setModel(
+          new JSONModel({
+            globalFilter: "",
+            availabilityFilterOn: false,
+            cellFilterOn: false,
+          }),
+          "ui"
+        );
+
+        this._oGlobalFilter = null;
+        this._oPriceFilter = null;
+
+        sap.ui.require(
+          ["sap/ui/table/sample/TableExampleUtils"],
+          function (TableExampleUtils) {
+            var oTb = oView.byId("infobar");
+            oTb.addContent(new ToolbarSpacer());
+            oTb.addContent(
+              TableExampleUtils.createInfoButton(
+                "sap/ui/table/sample/Filtering"
+              )
+            );
+          },
+          function (oError) {
+            /*ignore*/
+          }
+        );
+      },
+
+      initSampleDataModel: function () {
+        var oModel = new JSONModel();
+
+        var oDateFormat = DateFormat.getDateInstance({
+          source: { pattern: "timestamp" },
+          pattern: "dd/MM/yyyy",
+        });
+
+        jQuery.ajax(sap.ui.require.toUrl("task/shanita/orders.json"), {
+          dataType: "json",
+          success: function (oData) {
+            oModel.setData(oData);
+          },
+          error: function () {
+            Log.error("failed to load json");
+          },
+        });
+
+        return oModel;
+      },
+      onCreatePressed: function () {},
+      _filter: function () {
+        var oFilter = null;
+
+        if (this._oGlobalFilter && this._oPriceFilter) {
+          oFilter = new Filter([this._oGlobalFilter, this._oPriceFilter], true);
+        } else if (this._oGlobalFilter) {
+          oFilter = this._oGlobalFilter;
+        } else if (this._oPriceFilter) {
+          oFilter = this._oPriceFilter;
+        }
+
+        this.byId("table").getBinding().filter(oFilter, "Application");
+      },
+
+      filterGlobally: function (oEvent) {
+        var sQuery = oEvent.getParameter("query");
+        this._oGlobalFilter = null;
+
+        if (sQuery) {
+          this._oGlobalFilter = new Filter(
+            [
+              new Filter("Name", FilterOperator.Contains, sQuery),
+              new Filter("Category", FilterOperator.Contains, sQuery),
+            ],
+            false
+          );
+        }
+
+        this._filter();
+      },
+
+      filterPrice: function (oEvent) {
+        var oColumn = oEvent.getParameter("column");
+        if (oColumn != this.byId("price")) {
+          return;
+        }
+
+        oEvent.preventDefault();
+
+        var sValue = oEvent.getParameter("value");
+
+        function clear() {
+          this._oPriceFilter = null;
+          oColumn.setFiltered(false);
+          this._filter();
+        }
+
+        if (!sValue) {
+          clear.apply(this);
+          return;
+        }
+
+        var fValue = null;
+        try {
+          fValue = parseFloat(sValue, 10);
+        } catch (e) {
+          // nothing
+        }
+
+        if (!isNaN(fValue)) {
+          this._oPriceFilter = new Filter(
+            "Price",
+            FilterOperator.BT,
+            fValue - 20,
+            fValue + 20
+          );
+          oColumn.setFiltered(true);
+          this._filter();
+        } else {
+          clear.apply(this);
+        }
+      },
+
+      clearAllFilters: function (oEvent) {
+        var oTable = this.byId("table");
+
+        var oUiModel = this.getView().getModel("ui");
+        oUiModel.setProperty("/globalFilter", "");
+        oUiModel.setProperty("/availabilityFilterOn", false);
+
+        this._oGlobalFilter = null;
+        this._oPriceFilter = null;
+        this._filter();
+
+        var aColumns = oTable.getColumns();
+        for (var i = 0; i < aColumns.length; i++) {
+          oTable.filter(aColumns[i], null);
+        }
+      },
+
+      toggleAvailabilityFilter: function (oEvent) {
+        this.byId("availability").filter(
+          oEvent.getParameter("pressed") ? "X" : ""
+        );
+      },
+
+      formatAvailableToObjectState: function (bAvailable) {
+        return bAvailable ? "Success" : "Error";
       },
     });
   }
